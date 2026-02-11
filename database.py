41b0f5a60a486d16b0e21f9ecd8b37318de2090a
t1ag0pk <t1ag0123g.m@gmail.com>
2026-02-11 15:57:09 -0300

    Criação banco de dados

---
+++ database.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+def inicializar_banco():
+
+    # 1. Estabelece a conexão (cria o arquivo se não existir)
+    conexao = sqlite3.connect('controle_abastecimento.db')
+    
+    # 2. Cria o cursor para interagir com o banco
+    cursor = conexao.cursor()
+    
+    # 3. O comando SQL para criar a tabela
+    # Formato: nome_da_coluna TIPO_DE_DADO
+    sql = """
+    CREATE TABLE IF NOT EXISTS abastecimentos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        data TEXT NOT NULL,
+        km REAL NOT NULL,
+        valor_litro REAL NOT NULL,
+        quantidade_litros REAL NOT NULL,
+        valor_total REAL NOT NULL
+    )
+    """
+    
+    # 4. Executa o comando
+    cursor.execute(sql)
+    
+    # 5. Salva (commit) e fecha
+    conexao.commit()
+    conexao.close()
+
+def salvar_abastecimento(data, km, valor_litro, litros):
+
+    conexao = sqlite3.connect('controle_abastecimento.db')
+    cursor = conexao.cursor()
+
+    inserir = "INSERT INTO abastecimentos (data, km, valor_litro, quantidade_litros, valor_total) VALUES (?, ?, ?, ?, ?)"
+    dados = (data, km, valor_litro, litros, (valor_litro*litros))
+
+    cursor.execute(inserir, dados)
+    conexao.commit()
+    conexao.close()
+
+
+if __name__ == "__main__":
+    inicializar_banco()
+    print("Banco de dados inicializado com sucesso!")
