40b2b61a4ffd5b7e6b2e4d1c67c2a9bd140904b6
t1ag0pk <t1ag0123g.m@gmail.com>
2026-02-24 18:36:39 -0300

    estava esqueçendo de enviar o Main, função de delete quase finalizada.

--- Main.py
+++ Main.py
@@ -1,68 +1,243 @@
-# Desenvolvido por mim durante o primeiro semestre da faculdade
-# Basicamente não sabia utilizar nem criar funçoes mas fiz funcionar
-# Chegou a hora de colocar no git hub e melhora-lo
-
-
+from database import salvar_abastecimento, buscar_dados, busca_por_id, excluir_dados
 from datetime import datetime, timedelta
 
-dia_abastecimento_inicial= (input("\ninforme o dia do inicio do controle de abastecimento, formato(dd/mm/aaaa):"))
-km_abastecimento_inicial = int(input("informe a quantidade de km rodados do dia do inicio do controle de abastecimento:"))
+class Cor:
+    VERMELHO = '\033[91m'
+    VERDE = '\033[92m'
+    AMARELO = '\033[93m'
+    AZUL = '\033[94m'
+    CIANO = '\033[96m'
+    RESET = '\033[0m' 
+
+#entradas
+
+def verificar_entrada(mensagem):
+    while True:
+        try:
+            valor = float(input(mensagem).replace(',', '.'))
+            if valor <= 0:
+                print(f"{Cor.VERMELHO}Erro: A quantidade deve ser maior que zero.{Cor.RESET}")
+                continue 
+            else:
+                return valor
+        except ValueError:
+            print(f"{Cor.VERMELHO}digite apenas números{Cor.RESET}")
+
+def pedir_data():
+    while True:
+        try:
+            entrada = input(f"Informe o dia do abstecimento {Cor.AMARELO}(dd/mm/aaaa):{Cor.RESET} ")
+            return datetime.strptime(entrada, "%d/%m/%Y").strftime("%Y-%m-%d")
+        except ValueError:
+            print(f"{Cor.VERMELHO}Formato inválido! Use dd/mm/aaaa.{Cor.RESET}")
+
+def pedir_km():
+    while True:
+        try:
+            v = int(input("Informe a quilometragem total do dia do abastecimento: "))
+            if v < 0: raise ValueError
+            return v
+        except ValueError:
+            print(f"{Cor.VERMELHO}Digite um número inteiro positivo.{Cor.RESET}")
+
+def adicionar_dados():
+    data = pedir_data()
+    kms = pedir_km()
+    valor_litro = verificar_entrada("Informe o valor por litro: ")
+    litros = verificar_entrada("Informe quantos litros foram abastecidos: ")
+
+    salvar_abastecimento(data, kms, valor_litro, litros)
+    print(f"{Cor.VERDE}Informações salvas{Cor.RESET}")
 
 
-dia_abastecimento_bonus = dia_abastecimento_inicial
-dia_abastecimento_bonusX = datetime.strptime(dia_abastecimento_bonus, "%d/%m/%Y") 
-km_abastecimento_inicial_bonus = km_abastecimento_inicial
-litros_bonus = 0
+#processamento / saída
 
-dia_abastecimento_final= (input("informe o dia do final do controle de abastecimento, formato(dd/mm/aaaa):"))
-km_abastecimento_final = int(input("informe a quantidade de km rodados do dia do final do controle de abastecimento:"))
-
-litros = float(input("digite quantos litros foram abastecidos:"))
-litros_bonus = litros_bonus + litros
-
-
-
-
-dia_abastecimento_inicialX = datetime.strptime(dia_abastecimento_inicial , "%d/%m/%Y") 
-dia_abastecimento_finalX = datetime.strptime(dia_abastecimento_final , "%d/%m/%Y")
-
-total_dias_rodados = dia_abastecimento_finalX - dia_abastecimento_inicialX
-km_rodados = km_abastecimento_final - km_abastecimento_inicial
-media = km_rodados/litros
-
-#saida
-print(f"\nDurante o período do dia {dia_abastecimento_inicial} até o dia {dia_abastecimento_final}:\nPassaram {total_dias_rodados.days} dias;\nUsou {litros} L de combustível;\nVocê rodou {km_rodados} Kms;\nCom uma média de {media:.2f}Km/L.\n")
-
-
-pergunta = int(input("quer colocar os dados do próximos mês?\n0 = Não\n1 = Sim\n"))
-
-while pergunta == 1:
+def exibir_relatorio_completo():
+    dados = buscar_dados("ORDER BY data ASC")
     
-    ultimo_dia_informado = dia_abastecimento_final
-    ultimo_dia_informadoX = dia_abastecimento_finalX
-    ultimo_km_informado = km_abastecimento_final
+    if not dados:
+        print(f"{Cor.VERMELHO}Nenhum dado encontrado.{Cor.RESET}")
+        return
 
-    dia_abastecimento_inicial = ultimo_dia_informado
-    dia_abastecimento_inicialX = ultimo_dia_informadoX
+    print(f"\n{Cor.AZUL}{'ID':<4} {'Data':^12} {'KM':^11} {'Preço/L':^8} {'Litros':^10} {'R$ Total':^10}{Cor.RESET}")
+    print("-" * 60)
+    
+    for registro in dados:
+        id_b, data_b, km_b, v_l, litros_b, v_t = registro
+        data = datetime.strptime(data_b, "%Y-%m-%d").strftime("%d/%m/%Y")
+        print(f"{id_b:<4} {data:^12} {km_b:^11.0f} {v_l:^8.2f} {litros_b:^10.3f} {v_t:^10.2f}")
 
-    dia_abastecimento_final= (input("\ninforme o dia do final do controle de abastecimento, formato(dd/mm/aaaa):"))
-    dia_abastecimento_finalX = datetime.strptime(dia_abastecimento_final , "%d/%m/%Y")
+def calcular_consumo(registro):
 
-    km_abastecimento_inicial = ultimo_km_informado
-    km_abastecimento_final = int(input("informe a quantidade de km rodados do dia do final do controle de abastecimento:"))
+    dados = buscar_dados("ORDER BY data ASC") 
+    if len(dados) == 1:
+        print(f"{Cor.RESET}Você não possui dados suficientes para fazer o cálculo.\núnico abastecimento encontrado é:{Cor.RESET}")
+        exibir_relatorio_completo()
+        return
+    
+    elif len(dados) == 0:
+        print(f"{Cor.VERMELHO}Você não possui dados suficientes para fazer o cálculo.\nnenhum abastecimento encontrado{Cor.RESET}")
+        return
+    
+    elif registro == "ultimo":
+        # indíces negativos são os ultimos que foram adicionados ao banco
+        id_u, data_u, km_u, pl_u, litros_u, vt_u = dados[-1]
+        id_p, data_p, km_p, pl_p, litros_p, vt_p = dados[-2]
 
-    litros = float(input("digite quantos litros foram abastecidos:"))
-    litros_bonus = litros_bonus + litros
+        distancia = km_u - km_p
+        consumo_medio = distancia / litros_u
+        m_valor = pl_p - pl_u
+        if m_valor == 0:
+            textov = f"mesmo valor"
+        elif m_valor > 0:
+            textov = f"abaixou: {m_valor:.2f}"
+        else:
+            textov = f"subiu: {abs(m_valor):.2f}"
 
-    total_dias_rodados = dia_abastecimento_finalX - dia_abastecimento_inicialX
-    km_rodados = km_abastecimento_final - km_abastecimento_inicial
-    media = km_rodados/litros
+        data_objeto_u = datetime.strptime(data_u, "%Y-%m-%d")
+        data_objeto_p = datetime.strptime(data_p, "%Y-%m-%d")
+        data_formatada_u = datetime.strptime(data_u, "%Y-%m-%d").strftime("%d/%m/%Y")
+        data_formatada_p = datetime.strptime(data_p, "%Y-%m-%d").strftime("%d/%m/%Y")
+        dias_percorridos = (data_objeto_u - data_objeto_p).days
 
-    print(f"\nDurante o período do dia {dia_abastecimento_inicial} até o dia {dia_abastecimento_final}:\nPassaram {total_dias_rodados.days} dias;\nUsou {litros} L de combustível;\nVocê rodou {km_rodados} Kms;\nCom uma média de {media:.2f}Km/L.\n")
-    pergunta = int(input("quer colocar os dados do próximos mês?\n0 = Não\n1 = Sim\n"))
+        print(f"\n{Cor.AZUL}{'Data':^12} {'KM':^11} {'Preço/L':^8} {'Litros':^10} {'R$ Total':^10}{Cor.RESET}")
+        print("-" * 54)
+
+        # Print do penúltimo (-2)
+        print(f"{data_formatada_p:^12} {km_p:^11.0f} {pl_p:^8.2f} {litros_p:^10.3f} {vt_p:^10.2f}")
+        # Print do último (-1)
+        print(f"{data_formatada_u:^12} {km_u:^11.0f} {pl_u:^8.2f} {litros_u:^10.3f} {vt_u:^10.2f}")
+
+        print(f"\n{Cor.CIANO}{'Dias Percorridos':^20} {'KM Rodados':^11} {'Preço':^12} {'Média Km/l':^10}{Cor.RESET}")
+        print("-" * 56)
+        print(f"{dias_percorridos:^20} {distancia:^11.0f} {textov:^12} {consumo_medio:^10.2f}")
 
 
-km_rodados_bonus = km_abastecimento_final - km_abastecimento_inicial_bonus
-media_bonus = km_rodados_bonus/litros_bonus
-total_dias_rodados_bonus = dia_abastecimento_finalX - dia_abastecimento_bonusX
-print(f"\nBônus:\nDurante o período do dia {dia_abastecimento_bonus} até o dia {dia_abastecimento_final}:\nPassaram {total_dias_rodados_bonus.days} dias;\nUsou {litros_bonus:.2f}L de combustível;\nVocê rodou {km_rodados_bonus} Kms;\nCom uma média de {media_bonus:.2f}Km/L.\n")
+
+    elif registro == "todos":
+        for i in range(1, len(dados)):
+        
+            #o atual (i) e o anterior (i - 1)
+            id_u, data_u, km_u, pl_u, litros_u, vt_u = dados[i]
+            id_p, data_p, km_p, pl_p, litros_p, vt_p = dados[i-1]
+
+            distancia = km_u - km_p
+            consumo_medio = distancia / litros_u
+
+            m_valor = pl_p - pl_u
+            if m_valor == 0:
+                textov = f"mesmo valor"
+            elif m_valor > 0:
+                textov = f"abaixou: {m_valor:.2f}"
+            else:
+                textov = f"subiu: {abs(m_valor):.2f}"
+
+            data_objeto_u = datetime.strptime(data_u, "%Y-%m-%d")
+            data_objeto_p = datetime.strptime(data_p, "%Y-%m-%d")
+            data_formatada_u = datetime.strptime(data_u, "%Y-%m-%d").strftime("%d/%m/%Y")
+            data_formatada_p = datetime.strptime(data_p, "%Y-%m-%d").strftime("%d/%m/%Y")
+            dias_percorridos = (data_objeto_u - data_objeto_p).days
+        
+
+            if i == 1:
+                print(f"\n{Cor.CIANO}{"Data":^12}{'Dias Percorridos':^20} {'KM Rodados':^11} {'Preço':^12} {'Média Km/l':^10}{Cor.RESET}")
+                print("-" * 68)
+
+            print(f"{data_formatada_u:^12}{dias_percorridos:^20} {distancia:^11.0f} {textov:^12} {consumo_medio:^10.2f}")
+
+
+
+    elif registro == "total":
+        id_u, data_u, km_u, pl_u, litros_u, vt_u = dados[-1]
+        id_p, data_p, km_p, pl_p, litros_p, vt_p = dados[0]
+
+        litros_totais = sum(r[4] for r in dados[1:])
+        distancia = km_u - km_p
+        consumo_medio = distancia / litros_totais
+        m_valor = pl_p - pl_u
+        
+        if m_valor == 0:
+            textov = f"mesmo valor"
+        elif m_valor > 0:
+            textov = f"abaixou: {m_valor:.2f}"
+        else:
+            textov = f"subiu: {abs(m_valor):.2f}"
+
+        data_objeto_u = datetime.strptime(data_u, "%Y-%m-%d")
+        data_objeto_p = datetime.strptime(data_p, "%Y-%m-%d")
+        data_formatada_u = datetime.strptime(data_u, "%Y-%m-%d").strftime("%d/%m/%Y")
+        data_formatada_p = datetime.strptime(data_p, "%Y-%m-%d").strftime("%d/%m/%Y")
+        dias_percorridos = (data_objeto_u - data_objeto_p).days
+
+    
+        print(f"\n{Cor.CIANO}{"Inicio":^12}{"Final":^12}{'Dias Percorridos':^20} {'KM Rodados':^11} {'Preço':^12} {'Média Km/l':^10}{Cor.RESET}")
+        print("-" * 80)
+        print(f"{data_formatada_p:^12}{data_formatada_u:^12}{dias_percorridos:^20} {distancia:^11.0f} {textov:^12} {consumo_medio:^10.2f}")
+
+def excluir_abastecimento():
+
+    print(f"{Cor.AMARELO}Qual dos seguintes abastecimentos você deseja deletar?: {Cor.RESET}")
+    exibir_relatorio_completo()
+    print()
+
+    while True: #pegar o id de forma segura recebendo um número
+        try:
+            id_usuario = int(input(f"Informe o ID referente aos dados que você deseja deletar: "))
+            if id_usuario <= 0:
+                print(f"{Cor.VERMELHO}Erro: O número do ID deve ser maior que zero.{Cor.RESET}")
+                continue 
+            else:
+                break
+        except ValueError:
+            print(f"{Cor.VERMELHO}digite apenas números{Cor.RESET}")
+    
+    dados_do_abastecimento = busca_por_id(id_usuario)
+    if not dados_do_abastecimento:
+        print(f"{Cor.AMARELO}\nO valor de id informado não foi encontrado no banco, por favor, tente novamente.{Cor.RESET}")
+        print(f"{Cor.VERMELHO}Operação cancelada!{Cor.RESET}")
+        return
+    
+
+    print(f"{Cor.AMARELO}Dados a serem deletados:{Cor.RESET}")
+    print(f"\n{Cor.AZUL}{'ID':<4} {'Data':^12} {'KM':^11} {'Preço/L':^8} {'Litros':^10} {'R$ Total':^10}{Cor.RESET}")
+    print("-" * 60)
+    id_b, data_b, km_b, v_l, litros_b, v_t = dados_do_abastecimento
+    data = datetime.strptime(data_b, "%Y-%m-%d").strftime("%d/%m/%Y")
+    print(f"{id_b:<4} {data:^12} {km_b:^11.0f} {v_l:^8.2f} {litros_b:^10.3f} {v_t:^10.2f}")
+
+
+def menu():
+    while True:
+        print(f"\n{Cor.AZUL}=== SISTEMA KM POR LITRO ==={Cor.RESET}")
+        print("1. Cadastrar Novo Abastecimento")
+        print("2. Ver Histórico Completo")
+        print("3. Ver Último Consumo")
+        print("4. Ver Médias de Todos os Trechos")
+        print("5. Ver a Média entre o primeiro e último abstecimento")
+        print("6. Remover dados")
+        print("7. Sair")
+        
+        opcao = input(f"\n{Cor.AMARELO}Escolha uma opção: {Cor.RESET}")
+
+        if opcao == "1":
+            adicionar_dados()
+        elif opcao == "2":
+            exibir_relatorio_completo()
+        elif opcao == "3":
+            calcular_consumo(registro="ultimo")
+        elif opcao == "4":
+            calcular_consumo(registro="todos")
+        elif opcao == "5":
+            calcular_consumo(registro="total")
+        elif opcao == "6":
+            print("excluir abastecimento sairá em breve")
+            excluir_abastecimento() 
+        elif opcao == "7":
+            print(f"{Cor.VERDE}fechando!{Cor.RESET}")
+            break
+        else:
+            print(f"{Cor.VERMELHO}Opção inválida!{Cor.RESET}")
+
+# Única linha fora de funções para iniciar tudo:
+if __name__ == "__main__":
+    menu()
